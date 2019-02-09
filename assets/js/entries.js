70f083a36482df7dd1587fcffe7e3eedb8588b92
billzeng2k <billzeng2k@gmail.com>
2019-02-09 17:58:58 -0500

    done everything for now

--- assets/js/entries.js
+++ assets/js/entries.js
@@ -4,24 +4,27 @@ const games = [
         title: 'Quick Maths',
         image: './images/quick_maths.png',
         subtitle: 'Fall 2018',
-        description: 'Hello',
-        href: 'https://www.facebook.com/instantgames/2130997363884439'
-    },
-    {
-        type: 'entry',
-        title: 'Shape Shooter',
-        image: './images/shape_shooter.png',
-        subtitle: 'Winter 2017',
-        description: 'Hello',
-        href: 'https://play.google.com/store/apps/details?id=com.BillZeng2k.Shapes'
+        description: '<p>Challenge your friends to an epic battle of wits!</p><p>Solidify your position as the alpha problem solver!</p><p>Duke it out in a round of Quick Maths!</p>',
+        href: 'https://www.facebook.com/instantgames/2130997363884439',
+        blurb: 'Facebook Instant Games'
     },
     {
         type: 'entry',
         title: 'Flastronauts',
         image: './images/flastronauts.png',
         subtitle: 'Summer 2017',
-        description: 'Hello',
-        href: 'http://ldjam.com/events/ludum-dare/39/flastronaut'
+        description: '<p>The Earth is quickly becoming overcrowded, we must colonize other planets to avoid any disasters.</p><p> It is your job to bring life to these planets, but be careful, some might not be happy with your presence on the planet. Are you up to the task? Good. </p><p>Remember to bring your flashlight.</p>',
+        href: 'http://ldjam.com/events/ludum-dare/39/flastronaut',
+        blurb: 'Ludum Dare Game Jam'
+    },
+    {
+        type: 'entry',
+        title: 'Shape Shooter',
+        image: './images/shape_shooter.png',
+        subtitle: 'Winter 2017',
+        description: "<p>Geometry is hard, but who needs to understand shapes when you can just shoot them! </p><p>This 'Asteroids' inspired game has players collect power-ups and shoot different shapes all with unique properties. Watch out for the triangle, that one will get ya!</p>",
+        href: 'https://play.google.com/store/apps/details?id=com.BillZeng2k.Shapes',
+        blurb: 'Google Play Store'
     },
 ];
 
@@ -31,7 +34,7 @@ const jobs = [
         title: 'Ava Smart Gardens',
         image: './images/ava.png',
         subtitle: 'Winter 2017',
-        description: 'Hello',
+        description: '<p>Prototyped a web app that enabled users to monitor plant growth and fine-tune garden conditions via smart-garden sensors</p><p>The web app was implemented with NodeJs that connects to a data feed from Firebase</p>',
         blurb: 'Web Design Intern',
         href: 'https://www.avagrows.com'
     },
@@ -40,7 +43,7 @@ const jobs = [
         title: 'Triway Education',
         image: './images/triway.png',
         subtitle: 'Fall 2016 - Summer 2018',
-        description: 'Hello',
+        description: '<p>Taught over 30 students the fundamentals of programming and web design primarily using Java and HTML</p><p>Lead the robotics team to the provincial Vex Robotics competition</p>',
         blurb: 'Programming and Math Teacher',
         href: 'http://triwayeducation.com'
     },
@@ -49,7 +52,7 @@ const jobs = [
         title: 'Gifted People Services',
         image: './images/gps.png',
         subtitle: 'Summer 2017',
-        description: 'Hello',
+        description: '<p>Taught children the fundamentals of web design</p><p>Migrated the hosting server from Bluehost to goDaddy</p>',
         blurb: 'Web Design Intern and Teacher',
         href: 'https://giftedpeople.ca/'
     },
@@ -65,7 +68,7 @@ const education = [
         title: 'Canadian Computing Competition',
         image: './images/silver.png',
         subtitle: 'Summer 2018',
-        description: 'Hello',
+        description: "<p>Solved problems from topics ranging from</p><ul><li>Graph Theory (BFS, DFS, Dijkstra's, Prim's, Kruskal's)</li><li>Data Structures (Binary Search Tree, Binary Indexed Tree, Segment Tree, Disjoint Set)</li><li>Dynammic Programming</li></ul>",
         blurb: 'Silver Medalist'
     },
     {
@@ -73,7 +76,7 @@ const education = [
         title: 'Euclid Math Contest',
         image: './images/honour_roll.png',
         subtitle: 'Summer 2018',
-        description: 'Hello',
+        description: '<p>Solved problems from topics ranging from</p><ul><li>Algebra</li><li>Geometry</li><li>Calculus</li><li>Combinatorics</li></ul>',
         blurb: 'Honour Roll'
     },
     {
@@ -85,7 +88,7 @@ const education = [
         title: 'University of Waterloo',
         image: './images/waterloo.png',
         subtitle: 'Fall 2018 - Present',
-        description: 'Hello',
+        description: '<ul> <li> Pursuing a Bachelors of Computer Science </li> <li> Taking CS 145 (Advanced Computer Science) </li> <li> Culmative 4.0 GPA</li> </ul>',
         blurb: 'Bachelors of Computer Science'
     },
     {
@@ -93,7 +96,7 @@ const education = [
         title: 'Unionville High School',
         image: './images/uhs.png',
         subtitle: 'Fall 2014 - Summer 2018',
-        description: 'Hello',
+        description: '<ul> <li> Received the Computer Science award </li> <li> President of the Computer Science club in grade 12 </li> <li> Director of the Robotics club in grade 12 </li> </ul>',
     },
     {
         type: 'divider',
@@ -102,12 +105,17 @@ const education = [
     {
         type: 'entry',
         title: 'Languages',
-        description: 'Hello',
+        description: '<p>C++, C#, Java, Javascript, PHP, HTML, CSS, SQL, Dart</p>',
     },
     {
         type: 'entry',
         title: 'Frameworks',
-        description: 'Hello',
+        description: 'ReactJs, NodeJs, Instant Games API, Firebase, AWS',
+    },
+    {
+        type: 'entry',
+        title: 'Development Tools',
+        description: 'Unity, Git, Vim, Android Studio, Bash',
     },
     {
         type: 'divider',
@@ -115,17 +123,17 @@ const education = [
     },
     {
         type: 'entry',
-        title: 'Cancer Society',
-        subtitle: 'Summer 2014',
-        description: 'Hello',
-        image: './images/cancer_society.png'
+        title: 'Rotary Club',
+        description: 'By co-operating and leading my team, we made sure that the Markham ribfest operated smoothly',
+        subtitle: 'Fall 2017',
+        image: './images/rotary_club.png'
     },
     {
         type: 'entry',
-        title: 'Rotary Club',
-        description: 'Hello',
-        subtitle: 'Fall 2017',
-        image: './images/rotary_club.png'
+        title: 'Cancer Society',
+        subtitle: 'Summer 2014',
+        description: '<p>Collected donations by holding the door open and giving people a smile!</p>',
+        image: './images/cancer_society.png'
     },
 ];
 
@@ -139,21 +147,21 @@ const hobbies = [
         title: 'Tennis',
         image: './images/tennis.png',
         subtitle: '2008 - 2015',
-        description: 'Hello'
+        description: '<p>Qualified and competed in U14 Nationals</p><p>Placed 6th place in U14 provincials</p>'
     },
     {
         type: 'entry',
         title: 'Rock Climbing',
         image: './images/rock.png',
         subtitle: 'Fall 2016 - Present',
-        description: 'Hello'
+        description: '<p>Can climb V5s</p><p>No technique, only brute force</p>'
     },
     {
         type: 'entry',
         title: 'Basketball',
         image: './images/basketball.png',
         subtitle: 'Winter 2019 - Present',
-        description: 'Hello'
+        description: '<p>Intramural basketball at the University of Waterloo</p><p>Can only play defense</p>'
     },
     {
         type: 'divider',
@@ -163,19 +171,19 @@ const hobbies = [
         type: 'entry',
         title: 'Flat',
         image: './images/flat.png',
-        description: 'Hello'
+        description: '<p>Experienced in Photoshop and Illustrator</p><p>Primary artstyle</p>'
     },
     {
         type: 'entry',
         title: 'Pixel',
         image: './images/pixel.png',
-        description: 'Hello'
+        description: '<p>Can animate a walk cycle</p>'
     },
     {
         type: 'entry',
         title: 'Low Poly',
         image: './images/low_poly.png',
-        description: 'Hello'
+        description: '<p>Learning low poly just for fun!</p><p>Understands the basics of Blender, Maya and ZBrush</p>'
     },
     {
         type: 'divider',
@@ -185,7 +193,7 @@ const hobbies = [
         type: 'entry',
         title: 'Board Games',
         image: './images/board_games.png',
-        description: 'Hello'
+        description: '<p>Always inting</p>'
     },
 ];
 
@@ -195,6 +203,6 @@ const lol = [
         title: 'Thank You!',
         image: './images/face.png',
         subtitle: 'From now till forever <3',
-        description: "Thanks for viewing my website!</br>I had only one goal when I started developing this page and it was to create something that represented me</br>With this website I believe I created something only I could think of, and I'm really proud of how this turned out :)</br>I had a lot of fun creating this website, and I hope you had a lot of fun exploring it</br>OK THATS ALL, CYA LATER ALLIGATOR"
+        description: "<p>Thanks for viewing my website!</p><p>I had only one goal when I started developing this page and it was to create something that represented me. </br> With this website I believe I created something only I could think of, and I'm really proud of how this turned out :)</p><p>I had a lot of fun creating this website, and I hope you had a lot of fun exploring it</p><p>OK THATS ALL, CYA LATER ALLIGATOR</p>"
     },
 ];
