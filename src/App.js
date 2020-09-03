32da9c3368b0c94eafd1668b9e3aed57cd277fa7
billzeng2k <billzeng2k@gmail.com>
2020-09-03 10:51:31 -0400

    React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import Resume from './res/Resume.pdf';
 import './App.css';
+import './Intro.css';
+import './Kitchenmate.css';
+import km from './res/km.png';
+import Particles from 'react-particles-js';
+import { BookOutlined, GithubOutlined, MailOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="section intro">
+        <Particles params={{
+          particles: {
+            number: {
+              value: window.innerWidth < 600 ? 10 : 30 
+            },
+            size: {
+              value: window.innerWidth < 600 ? 1 : 2
+            }
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "repulse"
+              }
+            },
+            modes: {
+              repulse: {
+                distance: 400,
+                duration: 4
+              }
+            }
+          }
+        }}/>
+        <div className="heading"> Bill Zeng </div>
+        <div className="subheading"> Designer - Engineer - Innovator </div>
+        <div className="icon-container">
+          <a href={Resume} target="_blank"><BookOutlined className="icon"/></a>
+          <a href="https://github.com/billzeng2k" target="_blank"><GithubOutlined className="icon"/></a>
+          <a href="mailto:billzeng2k@gmail.com"><MailOutlined className="icon"/></a>
+          <a href="https://www.linkedin.com/in/billzeng2k/" target="_blank"><LinkedinOutlined className="icon"/></a>
+        </div>
+      </div>
+      {/* <div className="section kitchenmate">
+        <div className="center">
+          <img src={km} />
+        </div>
+      </div> */}
     </div>
   );
 }
